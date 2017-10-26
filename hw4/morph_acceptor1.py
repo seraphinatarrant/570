dc1c6fb40fa5844bdaab7f0f30c729d24eb9f11a
seraphinagoldfarb-tarrant <serif@uw.edu>
2017-10-26 16:41:08 -0700

    changed so that rather than printing it writes to a file passed as the third arg in the command line (the last commit did this for expand_fsm, this commit does it for morph_acceptor)

--- hw4/morph_acceptor1.py
+++ hw4/morph_acceptor1.py
@@ -13,28 +13,26 @@ import re
 if __name__ == "__main__":
     fsm = sys.argv[1]
     input_file = sys.argv[2]
-    quoted_file = 'quoted_strings'
+    output_file = sys.argv[3]
     line_dict = {}
     with open(input_file,'rU') as file:
         for line in file:
             if line:
                 new_line = re.sub(r'(.)', r'\1 ', line).strip()
                 line_dict[line.strip()] = new_line
-    #with open(quoted_file,'w') as file:
-    #    for line in line_list:
-    #        file.write(line)
-    for item in line_dict.items():
-        #print('Input {} Modified Input {}'.format(item[0],item[1]))
-        output = subprocess.check_output('echo {}|carmel {} -sli'.format(item[1], fsm),shell=True, universal_newlines=True,stderr=subprocess.STDOUT).split('\n')
-        input = item[0]
-        current_index = 0
-        for line in output:
-            input_string=re.search('(?<=Input line \d:).*', line)
-            if input_string:
-                next_line = output[current_index+1]
-                if '(0 states / 0 arcs)' in next_line:
-                    result = 'no'
-                else:
-                    result = 'yes'
-                print(input+' => '+result)
-            current_index+=1
+    with open(output_file,'w') as output_file:
+        for item in line_dict.items():
+            #print('Input {} Modified Input {}'.format(item[0],item[1]))
+            output = subprocess.check_output('echo {}|carmel {} -sli'.format(item[1], fsm),shell=True, universal_newlines=True,stderr=subprocess.STDOUT).split('\n')
+            input = item[0]
+            current_index = 0
+            for line in output:
+                input_string=re.search('(?<=Input line \d:).*', line)
+                if input_string:
+                    next_line = output[current_index+1]
+                    if '(0 states / 0 arcs)' in next_line:
+                        result = 'no'
+                    else:
+                        result = 'yes'
+                    output_file.write(input+' => '+result+'\n')
+                current_index+=1
--- hw4/morph_acceptor2.py
+++ hw4/morph_acceptor2.py
@@ -13,27 +13,29 @@ import re
 if __name__ == "__main__":
     fsm = sys.argv[1]
     input_file = sys.argv[2]
-    line_dict = {}
+    output_file = sys.argv[3]
+    line_dict = {} #consider not using a dict since then it won't be ordered...
     with open(input_file,'rU') as file:
         for line in file:
             if line:
                 new_line = re.sub(r'(.)', r'\1 ', line).strip()
                 line_dict[line.strip()] = new_line
-    for item in line_dict.items():
-        #print('Input {} Modified Input {}'.format(item[0],item[1]))
-        output = subprocess.check_output('echo {}|carmel {} -slibOE'.format(item[1], fsm),shell=True, universal_newlines=True,stderr=subprocess.STDOUT).split('\n')
-        input = item[0]
-        current_index = 0
-        for line in output:
-            #print(line)
-            input_string=re.search('(?<=Input line \d:).*', line)
-            if input_string:
-                next_line = output[current_index+1]
-                if '(0 states / 0 arcs)' in next_line:
-                    result = '*NONE*'
-                else:
-                    output_line = output[current_index+2]
-                    result =''.join(output_line.split()[:-1])
-                    result = re.sub(r'[|]',r' ',result).strip()
-                print(input+' => '+result)
-            current_index+=1
+    with open(output_file, 'w') as output_file:
+        for item in line_dict.items():
+            #print('Input {} Modified Input {}'.format(item[0],item[1]))
+            output = subprocess.check_output('echo {}|carmel {} -slibOE'.format(item[1], fsm),shell=True, universal_newlines=True,stderr=subprocess.STDOUT).split('\n')
+            input = item[0]
+            current_index = 0
+            for line in output:
+                #print(line)
+                input_string=re.search('(?<=Input line \d:).*', line)
+                if input_string:
+                    next_line = output[current_index+1]
+                    if '(0 states / 0 arcs)' in next_line:
+                        result = '*NONE*'
+                    else:
+                        output_line = output[current_index+2]
+                        result =''.join(output_line.split()[:-1])
+                        result = re.sub(r'[|]',r' ',result).strip()
+                    output_file.write(input+' => '+result+'\n')
+                current_index+=1
