7f154c4faf0fe437f132d8137bd7e14da90e09b6
seraphinagoldfarb-tarrant <serif@uw.edu>
2017-11-15 14:04:00 -0800

    Committing entire hw7 directory (hmm and viterbi)

--- hw7/conv_format.py
+++ hw7/conv_format.py
@@ -10,18 +10,18 @@ import sys
 
 def format_obs_state_pairs(inputline_list):
     #might want to consider doing this line by line
-    #also need to remove BOS_BOS so it doesn't fuck up the zip
     output_lines = []
     for line in inputline_list:
         obs, states = line.split('=>') #split observation and state sequence on the character delimiting them
         #the [:-1] below is cause the final entry will be logprob rather than a statename which we dont want to capture
-        obs_tokens, states_tokens = obs.split(), states.split()[:-1]
+        #starting states from 1 ensures it doesn't capture BOS_BOS
+        obs_tokens, states_tokens = obs.split(), states.split()[1:-1]
         #need to convert the list of states to a list of tags, which is the second tag in the state name
         tags = [state.split('_')[1] for state in states_tokens]
         zipped_tokens = zip(obs_tokens, tags)
         output_tokens = []
         [output_tokens.append('{}/{}'.format(pair[0], pair[1])) for pair in zipped_tokens]
-        output_lines.append('{}\n'.format(' '.join(output_tokens)))
+        output_lines.append('{}'.format(' '.join(output_tokens))) #if change to write to a file will want to put \n here
     for line in output_lines:
         print(line)
 
@@ -29,11 +29,16 @@ def format_obs_state_pairs(inputline_list):
 
 
 if __name__ == "__main__":
-    input_filename = sys.argv[1] #this will be removed for the cat pipe but is here for debugging
+    #input_filename = sys.argv[1] #this will be removed for the cat pipe but is here for debugging
+    input_data = sys.stdin.readlines()
     inputlines = []
-    with open(input_filename, 'rU') as infile:
-        for line in infile:
-            if line:
-                inputlines.append(line.strip())
+
+    #with open(input_filename, 'rU') as infile:
+    #    for line in infile:
+    #        if line:
+    #            inputlines.append(line.strip())
+    for line in input_data:
+        if line:
+            inputlines.append(line.strip())
     format_obs_state_pairs(inputlines)
     
